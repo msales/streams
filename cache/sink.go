a81d5a052cb81f536f0f1b3d990495ee6850ac64
Nicholas Wiersma <nick@wiersma.co.za>
2018-04-20 08:01:21 +0200

    Add cache sink

---
+++ cache/sink.go
@@ -0,0 +1,43 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/msales/pkg/cache"
+	"github.com/msales/streams"
+)
+
+type CacheSink struct {
+	ctx streams.Context
+
+	cache  cache.Cache
+	expire time.Duration
+}
+
+// NewCacheSink creates a new cache insert sink.
+func NewCacheSink(cache cache.Cache, expire time.Duration) *CacheSink {
+	return &CacheSink{
+		cache:  cache,
+		expire: expire,
+	}
+}
+
+// WithContext sets the context on the Processor.
+func (p *CacheSink) WithContext(ctx streams.Context) {
+	p.ctx = ctx
+}
+
+// Process processes the stream record.
+func (p *CacheSink) Process(key, value interface{}) error {
+	k := key.(string)
+
+	p.cache.Set(k, value, p.expire)
+	p.ctx.Commit()
+
+	return nil
+}
+
+// Close closes the processor.
+func (p *CacheSink) Close() error {
+	return nil
+}
