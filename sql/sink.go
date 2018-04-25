79dc04e0923a43a38a3ad78107533b92ff6c0862
Nicholas Wiersma <nick@wiersma.co.za>
2018-04-25 09:23:09 +0200

    Improve sql sink

--- sql/sink.go
+++ sql/sink.go
@@ -6,27 +6,52 @@ import (
 	"github.com/msales/streams"
 )
 
+type SqlTxFunc func(*sql.Tx) error
+
 type SqlInsertFunc func(*sql.Tx, interface{}, interface{}) error
+
+type SqlSinkFunc func(*SqlSink)
+
+func WithBeginFn(fn SqlTxFunc) SqlSinkFunc {
+	return func(s *SqlSink) {
+		s.beginFn = fn
+	}
+}
+
+func WithCommitFn(fn SqlTxFunc) SqlSinkFunc {
+	return func(s *SqlSink) {
+		s.commitFn = fn
+	}
+}
 
 type SqlSink struct {
 	ctx streams.Context
 
 	db *sql.DB
 	tx *sql.Tx
-	fn SqlInsertFunc
+
+	beginFn  SqlTxFunc
+	insertFn SqlInsertFunc
+	commitFn SqlTxFunc
 
 	batch int
 	count int
 }
 
 // NewSqlSink creates a new batch sql insert sink.
-func NewSqlSink(db *sql.DB, fn SqlInsertFunc, batch int) *SqlSink {
-	return &SqlSink{
-		db:    db,
-		fn:    fn,
-		batch: batch,
-		count: 0,
+func NewSqlSink(db *sql.DB, fn SqlInsertFunc, batch int, opts ...SqlSinkFunc) *SqlSink {
+	s := &SqlSink{
+		db:       db,
+		insertFn: fn,
+		batch:    batch,
+		count:    0,
 	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	return s
 }
 
 // WithContext sets the context on the Processor.
@@ -40,7 +65,7 @@ func (p *SqlSink) Process(key, value interface{}) error {
 		return err
 	}
 
-	if err := p.fn(p.tx, key, value); err != nil {
+	if err := p.insertFn(p.tx, key, value); err != nil {
 		return err
 	}
 
@@ -53,23 +78,39 @@ func (p *SqlSink) Process(key, value interface{}) error {
 	return nil
 }
 
-func (p *SqlSink) ensureTransaction() (err error) {
+func (p *SqlSink) ensureTransaction() error {
+	var err error
+
 	if p.tx != nil {
-		return
+		return nil
 	}
 
 	p.tx, err = p.db.Begin()
-	return
-}
-
-func (p *SqlSink) commitTransaction() (err error) {
-	if p.tx == nil {
-		return
+	if err != nil {
+		return err
 	}
 
-	if err = p.tx.Commit(); err != nil {
+	if p.beginFn != nil {
+		return p.beginFn(p.tx)
+	}
+
+	return nil
+}
+
+func (p *SqlSink) commitTransaction() error {
+	if p.tx == nil {
+		return nil
+	}
+
+	if p.commitFn != nil {
+		if err := p.commitFn(p.tx); err != nil {
+			return err
+		}
+	}
+
+	if err := p.tx.Commit(); err != nil {
 		p.tx.Rollback()
-		return
+		return err
 	}
 	p.tx = nil
 
